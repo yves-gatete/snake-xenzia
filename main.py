7bc2cc0775dc04c28b88f416fddaed5b335e4065
Yves Gatete <yves.h.gatete@gmail.com>
2025-09-04 23:02:25 -0700

    added modularity

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake
 
 #screen properties
 screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 screen.title("Snake Xenzia")
-
-#squares are 20 units length
-starting_positions = [(0,0), (-20,0), (-40,0)]
-for pos in starting_positions:  
-    segment = Turtle("square")
-    segment.color("white")
-    segment.setposition(pos)
+screen.tracer(0)
 
 
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 screen.exitonclick()
